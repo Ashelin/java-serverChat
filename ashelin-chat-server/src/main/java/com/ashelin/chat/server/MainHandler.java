809cb0024cff35defbe121548d987e10e0573d79
Danylo Shelest <daniel.shelest1@gmail.com>
2022-10-19 17:45:59 +0200

    Adding server notifications to chat

--- ashelin-chat-server/src/main/java/com/ashelin/chat/server/MainHandler.java
+++ ashelin-chat-server/src/main/java/com/ashelin/chat/server/MainHandler.java
@@ -1,9 +1,7 @@
 package com.ashelin.chat.server;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 import java.util.ArrayList;
@@ -19,28 +17,37 @@ public class MainHandler extends SimpleChannelInboundHandler<String> {
         channeLs.add(ctx.channel());
         clientName = "Client #" + newClientIndex;
         newClientIndex++;
+        broadcastMessage("SERVER", "Connected new client: " + clientName);
     }
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
+        System.out.println("New Message " + s);
         if(s.startsWith("/")){
             if(s.startsWith("/changename")) {
-                clientName = s.split("\\s", 2)[1];
+                String newNickname = s.split("\\s", 2)[1] ;
+                broadcastMessage("SERVER", "Client " + clientName + "Change his name to: " + newNickname);
+                clientName = newNickname;
             }
             return;
         }
-        System.out.println("New Message " + s);
-        String out = String.format("[%s]: %s\n", clientName, s);
+        broadcastMessage(clientName, s);
+
+    }
+
+    public void broadcastMessage(String clientName, String message) {
+
+        String out = String.format("[%s]: %s\n", clientName, message);
         for(Channel c : channeLs) {
             c.writeAndFlush(out);
         }
-
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        System.out.println("Client " + clientName + "leaved");
+        System.out.println("Client " + clientName + "dropped");
         channeLs.remove(ctx.channel());
+        broadcastMessage("SERVER", "Client " + clientName + "leaved channel");
         ctx.close();
     }
 }
